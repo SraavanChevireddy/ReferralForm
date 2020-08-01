0946bcae5346251fb7f806599d7c7c18dd4f9923
Sraavan Chevireddy <schevireddy@saventech.com>
2020-08-01 13:17:04 +0530

    Initial Commit

---
+++ ReferralForm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReferralForm
+//
+//  Created by Sraavan Chevireddy on 8/1/20.
+//  Copyright © 2020 Saven Developers Office. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
